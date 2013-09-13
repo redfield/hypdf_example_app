ff1c6177f60d4e6cc3ce34a2ec9729643e448a5a
redfield <up.redfield@gmail.com>
2013-09-13 18:20:25 +0600

    New features

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -1,6 +1,6 @@
 class PdfController < ApplicationController
 
-  def index
+  def htmltopdf_page
     @html = %Q{
       <html>
         <head><title>Example HTML</title></head>
@@ -8,7 +8,7 @@ class PdfController < ApplicationController
           <h1>HyPDF add-on</h1>
 
           <div style="background:#E1EAF3;border:1px solid #d0dbe6;border-radius: 3px;color:#4F535D;margin: 0 0 22px 0;padding:12px;">
-            HyPDF is an <a href="https://addons.heroku.com/hypdf">add-on</a> for easy and flexible creation of PDF from HTML.
+            HyPDF is an <a href="https://addons.heroku.com/hypdf">add-on</a> that provides you the full set of tools for working with PDF docuemnts.
           </div>
 
           <div style="margin:30px 0;-webkit-transform:rotate(5deg);">
@@ -39,16 +39,71 @@ class PdfController < ApplicationController
     }
   end
 
-  def create
+  def htmltopdf
     options = (params[:options] || {}).merge(test: true)
-    hypdf = HyPDF.new(params[:content], options)
 
     if params[:commit] == 'Download PDF'
-      send_data(hypdf.get, filename: 'hypdf_test.pdf', type: 'application/pdf')
+      hypdf = HyPDF.htmltopdf(params[:content], options)
+      send_data(hypdf[:pdf], filename: 'hypdf_test.pdf', type: 'application/pdf')
     else
-      # NOTE: replace 'hypdf_test' with your backet name
-      url = hypdf.upload_to_s3('hypdf_test', 'hypdf_test.pdf', true)
-      redirect_to :root, notice: "PDF url: #{url} | Number of pages: #{hypdf.meta[:pages]}"
+      options.merge!(
+        bucket: 'hypdf_test', # NOTE: replace 'hypdf_test' with your backet name
+        key: 'hypdf_test.pdf',
+        public: true
+      )
+      hypdf = HyPDF.htmltopdf(params[:content], options)
+      redirect_to '/htmltopdf', notice: "PDF url: #{hypdf[:url]} | Number of pages: #{hypdf[:pages]}"
+    end
+  end
+
+  def pdfinfo
+    file = params[:file]
+    hypdf = HyPDF.pdfinfo(file.path)
+    redirect_to '/pdfinfo', notice: "PDF info: #{hypdf}"
+  end
+
+  def pdftotext
+    file = params[:file]
+    options = (params[:options] || {})
+    hypdf = HyPDF.pdftotext(file.path, options)
+    @text = hypdf[:text]
+    render :pdftotext_page
+  end
+
+  def pdfextract
+    file = params[:file]
+    options = (params[:options] || {}).merge(test: true)
+
+    if params[:commit] == 'Download PDF'
+      hypdf = HyPDF.pdfextract(file.path, options)
+      send_data(hypdf[:pdf], filename: 'hypdf_test.pdf', type: 'application/pdf')
+    else
+      options.merge!(
+        bucket: 'hypdf_test', # NOTE: replace 'hypdf_test' with your backet name
+        key: 'hypdf_test.pdf',
+        public: true
+      )
+      hypdf = HyPDF.pdfextract(file.path, options)
+      redirect_to '/pdfextract', notice: "PDF url: #{hypdf[:url]}"
+    end
+  end
+
+  def pdfunite
+    file_1 = params[:file_1]
+    file_2 = params[:file_2]
+    options = {test: true}
+
+    if params[:commit] == 'Download PDF'
+      hypdf = HyPDF.pdfunite(file_1.path, file_2.path, options)
+      send_data(hypdf[:pdf], filename: 'hypdf_test.pdf', type: 'application/pdf')
+    else
+      options.merge!(
+        bucket: 'hypdf_test', # NOTE: replace 'hypdf_test' with your backet name
+        key: 'hypdf_test.pdf',
+        public: true
+      )
+      hypdf = HyPDF.pdfunite(file_1.path, file_2.path, options)
+      redirect_to '/pdfunite', notice: "PDF url: #{hypdf[:url]}"
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 HypdfExampleApp::Application.routes.draw do
-  get "pdf" => 'pdf#index'
-  post "pdf" => 'pdf#create'
+  get "htmltopdf" => 'pdf#htmltopdf_page'
+  post "htmltopdf" => 'pdf#htmltopdf'
+
+  get "pdfinfo" => 'pdf#pdfinfo_page'
+  post "pdfinfo" => 'pdf#pdfinfo'
+
+  get "pdftotext" => 'pdf#pdftotext_page'
+  post "pdftotext" => 'pdf#pdftotext'
+
+  get "pdfextract" => 'pdf#pdfextract_page'
+  post "pdfextract" => 'pdf#pdfextract'
+
+  get "pdfunite" => 'pdf#pdfunite_page'
+  post "pdfunite" => 'pdf#pdfunite'
 
   post 'async_pdf' => 'pdf#async_pdf'
 
-  root :to => 'pdf#index'
+  root :to => 'pdf#htmltopdf_page'
 end
